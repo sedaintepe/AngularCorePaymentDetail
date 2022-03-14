617c72675fe8107fcf12c0630e30c1e8939ab8b0
Seda <sedaintepe995@gmail.com>
2022-03-14 12:45:48 +0300

    Core ile bağlantı

---
+++ src/app/shared/payment-detail.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+
+import { PaymentDetail } from './payment-detail.model';
+import {HttpClient} from  '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaymentDetailService {
+
+  constructor(private http:HttpClient) { }
+  readonly baseUrl='https://localhost:44318/api/PaymentDetails'
+  formData:PaymentDetail=new PaymentDetail();
+  list:PaymentDetail[];
+
+  postPaymentDetail(){
+    return this.http.post(this.baseUrl,this.formData);
+  }
+  putPaymentDetail(){
+    return this.http.put(`${this.baseUrl}/${this.formData.paymentDetailId}`,this.formData);
+  }
+  deletePaymentDetail(id:number){
+    return this.http.delete(`${this.baseUrl}/${id}`);
+  }
+
+  refreshList(){
+    this.http.get(this.baseUrl)
+    .toPromise()
+    .then(res=>this.list=res as PaymentDetail[]);
+  }
+
+}
